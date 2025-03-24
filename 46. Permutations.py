7554c11ba3ccc27d15a268d38ed47b2fda0b9b16
jesronstark <162868061+jesronstark@users.noreply.github.com>
2025-03-24 16:35:41 +0530

    Create 46. Permutations.py

---
+++ 46. Permutations.py
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(result, nums, 0);
+        return result;
+    }
+
+    private void backtrack(List<List<Integer>> result, int[] nums, int start) {
+        if (start == nums.length) {
+            List<Integer> permutation = new ArrayList<>();
+            for (int num : nums) {
+                permutation.add(num);
+            }
+            result.add(permutation);
+            return;
+        }
+        for (int i = start; i < nums.length; i++) {
+            swap(nums, start, i);
+            backtrack(result, nums, start + 1);
+            swap(nums, start, i); // backtrack
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
